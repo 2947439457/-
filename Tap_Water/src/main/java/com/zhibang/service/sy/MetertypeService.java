1296d32709806a75de0b8a5dd3f0a5fdb84d828e
yjh <2947439457@qq.com>
2020-12-15 09:17:45 +0800

    update

---
+++ Tap_Water/src/main/java/com/zhibang/service/sy/MetertypeService.java
@@ -0,0 +1,11 @@
+package com.zhibang.service.sy;
+
+/**
+ * @author admin
+ * @version 1.0.0
+ * @ClassName MetertypeServiceImpl.java
+ * @Description TODO
+ * @createTime 2020年12月15日 09:16:00
+ */
+public interface MetertypeService {
+}
---
+++ Tap_Water/src/main/java/com/zhibang/service/sy/impl/MetertypeServiceImpl.java
@@ -0,0 +1,11 @@
+package com.zhibang.service.sy.impl;
+
+/**
+ * @author admin
+ * @version 1.0.0
+ * @ClassName MetertypeServiceImpl.java
+ * @Description TODO
+ * @createTime 2020年12月15日 09:17:00
+ */
+public class MetertypeServiceImpl {
+}
